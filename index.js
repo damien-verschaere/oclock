521751d5239880fc23000715eff84d7d673478ce
damien-verschaere <damien.verschaere@laplateforme.io>
2022-04-25 06:53:43 +0200

    timer 90%

--- index.js
+++ index.js
@@ -1,147 +1,162 @@
-document.addEventListener("DOMContentLoaded",()=>{
-const start = document.querySelector("#start")
-const stop = document.querySelector("#stop")
-const tour = document.querySelector("#tour")
-const reset = document.querySelector("#reset")
-const timerMinute = document.querySelector("#minute")
-const timerSeconde = document.querySelector("#seconde")
-const validation = document.querySelector("#valide")
-var centieme = 0 
-var dixieme = 0 
-var seconde =0 
-var minute = 0 
-var afficher
-var sec_
-var parametre
-var temps
+document.addEventListener("DOMContentLoaded", () => {
+    const start = document.querySelector("#start")
+    const stop = document.querySelector("#stop")
+    const tour = document.querySelector("#tour")
+    const reset = document.querySelector("#reset")
+    const timerMinute = document.querySelector("#minute")
+    const timerSeconde = document.querySelector("#seconde")
+    const validation = document.querySelector("#valide")
+    const timer = document.querySelector("#timer")
+    var centieme = 0
+    var dixieme = 0
+    var seconde = 0
+    var minute = 0
+    var afficher
+    var sec_
+    var parametre
+    var temps
 
-function horloge(){
-const date=new Date;
-var heure = date.getHours();
-var minutes =date.getMinutes();
-var secondes = date.getSeconds();
-heure = (heure < 10) ? "0" + heure : heure;
-minutes = (minutes < 10) ? "0" + minutes : minutes;
-secondes = (secondes < 10) ? "0" + secondes : secondes;
-var innerH = document.querySelector('#horloge');
-var time = `${heure}:${minutes}:${secondes}`;
-// console.log(timvar
-innerH.innerHTML=time;
-setTimeout(horloge,1000);
-}
+    function horloge() {
+        const date = new Date;
+        var heure = date.getHours();
+        var minutes = date.getMinutes();
+        var secondes = date.getSeconds();
+        heure = (heure < 10) ? "0" + heure : heure;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        secondes = (secondes < 10) ? "0" + secondes : secondes;
+        var innerH = document.querySelector('#horloge');
+        var time = `${heure}:${minutes}:${secondes}`;
+        // console.log(timvar
+        innerH.innerHTML = time;
+        setTimeout(horloge, 1000);
+    }
+    function decompteTimer(){
+        gettime(325)
 
-function chronometer(){
+        setInterval(function () {
+            seconde--;
+            if (seconde<= 0) {
+                seconde = 59;
 
-        setInterval(function (){
-            centieme++;
-                if (centieme > 9) {
-                    centieme = 0;
-                    
-                }
+            }
         }, 1);
-        
+        minute--
+    }
+    function chronometer() {
+
+        setInterval(function () {
+            centieme++;
+            if (centieme > 9) {
+                centieme = 0;
+
+            }
+        }, 1);
+
         dixieme++;
-        dixieme*10;
-       
+        dixieme * 10;
+
         if (dixieme > 9) {
             dixieme = 0;
             seconde++;
-        }  
+        }
 
         if (seconde < 10) {
             sec_ = "0" + seconde;
         }
-        if (seconde>=60) {
-            seconde=0
+        if (seconde >= 60) {
+            seconde = 0
             minute++
         }
         else {
             sec_ = seconde;
         }
-            
-        afficher = minute +":"+ sec_ + ":" + dixieme + centieme;
-       temps= document.getElementById("chrono").innerHTML = afficher;
-        
-     parametre = setTimeout(chronometer,100);
-    
-}
 
-function debut(){
-    start.addEventListener("click",chronometer)
-}
+        afficher = minute + ":" + sec_ + ":" + dixieme + centieme;
+        temps = document.getElementById("chrono").innerHTML = afficher;
 
-function arret(){
-    stop.addEventListener("click",()=>{
-        clearTimeout(parametre)
-    })
+        parametre = setTimeout(chronometer, 100);
 
-}
-function enregistrementTour(){
-    tour.addEventListener("click",()=>{
-        let tps =document.querySelector("#tps")
-        tps.innerHTML=temps
-    })
-}
+    }
 
-function reinitialiser(){
-    reset.addEventListener("click",()=>{
-        clearInterval()
-        centieme = 0 
-        dixieme = 0 
-        seconde=0 
-        minute = 0 
-        afficher = minute +":"+ seconde + ":" + dixieme + centieme;
-        temps= document.getElementById("chrono").innerHTML = afficher;
-        
-    })
-}
-function gettime(){
-    validation.addEventListener("click",()=>{
-        seconde= timerSeconde.value
-        minute=  timerMinute.value
-        time = minute + ":" + seconde
-        if(seconde<0){
-            alert("le temps negatif est impossible")
-        }
-        if(seconde>59){
-            alert("60 secondes egale une minute")
-            minute ++ 
-            seconde = seconde %60
+    function debut() {
+        start.addEventListener("click", chronometer)
+    }
+
+    function arret() {
+        stop.addEventListener("click", () => {
+            clearTimeout(parametre)
+        })
+
+    }
+    function enregistrementTour() {
+        tour.addEventListener("click", () => {
+            let tps = document.querySelector("#tps")
+            tps.innerHTML = temps
+        })
+    }
+
+    function reinitialiser() {
+        reset.addEventListener("click", () => {
+            clearInterval()
+            centieme = 0
+            dixieme = 0
+            seconde = 0
+            minute = 0
+            afficher = minute + ":" + seconde + ":" + dixieme + centieme;
+            temps = document.getElementById("chrono").innerHTML = afficher;
+
+        })
+    }
+    function gettime() {
+        validation.addEventListener("click", () => {
+            seconde = timerSeconde.value
+            minute = timerMinute.value
+
             time = minute + ":" + seconde
-            
-        }
-        // if (seconde>60) {
-        //     console.log(seconde)
-        //     var calc = seconde % 60 
-        //     // console.log(seconde)
-        //     // console.log(calc)
-        //     result = seconde - calc
-        //     console.log(result)
-        //     if (result>60) {
-        //         var calcMinute = result / 60
-        //     }
-        //     var minuteTime = minute + calcMinute
-        //     time = calcMinute + ":" + calc
-        // }
-        // else{
-        // time = minute + ":" + seconde
-        // }
-        console.log(time)
-        
-    })
-   
-    
-}
+            if (minute===null && seconde===null ) {
+                alert("veuillez rentrer une valeur ")
+            }
+            if (seconde < 0) {
+                alert("le temps negatif est impossible")
+            }
+            if (seconde > 60) {
+                console.log(seconde)
+                var calc = seconde % 60
+                // console.log(seconde)
+                // console.log(calc)
+                result = seconde - calc
+                // console.log(result + "seconde?")
+                if (result > 60) {
+
+                    var calcMinute = result / 60
+                    var resuCalc = parseInt(minute) + parseInt(calcMinute)
+                    if (isNaN(parseInt(minute))) {
+                        if (true) {
+                            resuCalc = calcMinute
+                        }
+                    }
+                    // console.log(resuCalc)
+                    time = resuCalc + ":" + calc
+                }
+
+            }
+            else {
+                time = minute + ":" + seconde
+            }
+            // console.log(time)
+            timer.innerHTML = time
+        })
 
 
+    }
 
-gettime()
-// add10s()
-// minuteur();
-reinitialiser()
-enregistrementTour()
-arret()
-debut()
-horloge()
+
+    decompteTimer()
+    // gettime()
+    reinitialiser()
+    enregistrementTour()
+    arret()
+    debut()
+    horloge()
 
 })
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,32 +9,39 @@
     <link rel="stylesheet" href="index.css">
     <title>Document</title>
 </head>
+
 <body>
     <header>
         <h1>OCLOCK</h1>
     </header>
     <main>
-    <div class="heure">
-        <h2>HORLOGE</h2>
-        <p id="horloge"></p>
-    </div>
-    <div class="chronometre">
-        <h2>CHRONOMETRE</h2>
-        <p id="chrono"></p>
-        <div class=button>
-        <button id="start">start</button>
-        <button id="stop">stop</button>
-        <button id="tour">tour</button>
-        <button id="reset">reset</button>
-        </div> 
-        <div>
-            <p id="tps"></p>
+        <div class="heure">
+            <h2>HORLOGE</h2>
+            <p id="horloge"></p>
         </div>
-    </div>
-        <h2>MINUTEUR</h2>
-        <input type="number" id="minute">
-        <input type="number" id="seconde" min=1 max=59>
-        <button id="valide">validez le temps </button>
+        <div class="chronometre">
+            <h2>CHRONOMETRE</h2>
+            <p id="chrono"></p>
+            <div class=button>
+                <button id="start">start</button>
+                <button id="stop">stop</button>
+                <button id="tour">tour</button>
+                <button id="reset">reset</button>
+            </div>
+            <div>
+                <p id="tps"></p>
+            </div>
+        </div>
+        <div id="minuteur">
+            <h2>MINUTEUR</h2>
+            <p id="timer"></p>
+            <div class="button">
+                <input type="number" id="minute" placeholder="minute">
+                <input type="number" id="seconde" placeholder="seconde">
+                <button id="valide">validez le temps </button>
+            </div>
+        </div>
     </main>
 </body>
+
 </html>
